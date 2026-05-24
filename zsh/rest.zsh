ee3274e7a0e4537cc0bba7eb83a3c21338323997
turtNova <gudturtleboi@gmail.com>
2026-05-24 22:02:02 +0200

    %zshrc

--- zsh/rest.zsh
+++ zsh/rest.zsh
@@ -8,7 +8,7 @@ fi
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
-bindkey -A viins main
+#bindkey -A viins main
 
 function run-editzshrc { nvim ~/.zshrc }
 zle -N run-editzshrc
