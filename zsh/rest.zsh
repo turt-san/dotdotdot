c14dc56394b8b1f370a8a13b0c50559f3d0475d8
turtNova <gudturtleboi@gmail.com>
2026-06-26 02:21:20 +0200

    %

--- zsh/rest.zsh
+++ zsh/rest.zsh
@@ -7,6 +7,8 @@ if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]
     source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 fi
 
+source /usr/share/nvm/init-nvm.sh
+
 # To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
 [[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh
 
